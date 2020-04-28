cd64c955bc7090d1491e3372ee7804076eca3c76
Bryce Klinker <klinker.bryce@principal.com>
2020-04-28 10:24:26 -0500

    Reverting test name

--- test-driven-development/bowling-game/java/src/test/java/bowling/game/BowlingGameTest.java
+++ test-driven-development/bowling-game/java/src/test/java/bowling/game/BowlingGameTest.java
@@ -9,8 +9,9 @@ import static org.junit.Assert.*;
 
 public class BowlingGameTest {
     @Test
-    public void whenRollingAllGutterBallsThenScoreIsZero() {
+    public void testSomething() {
         BowlingGame game = new BowlingGame();
+
         assertEquals(0, game.score());
     }
 }
